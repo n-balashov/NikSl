34e137dfb1b472b5340dbd252174ac72499b6f79
Pavel Sidorovich <sidorovichpavelalex@gmail.com>
2021-03-13 12:19:06 +0300

    feat: add bottom navigation

--- app/src/main/java/com/pavesid/niksl/MainActivity.kt
+++ app/src/main/java/com/pavesid/niksl/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.pavesid.niksl
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.pavesid.niksl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.like -> {
+                    Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.home -> {
+                    Toast.makeText(this, "2", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.dislike -> {
+                    Toast.makeText(this, "3", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        binding.bottomNavigation.setOnNavigationItemReselectedListener { item ->
+            when (item.itemId) {
+                R.id.like -> {
+                    Toast.makeText(this, "Уже нажато", Toast.LENGTH_SHORT).show()
+                }
+                R.id.home -> {
+                    Toast.makeText(this, "Уже нажато", Toast.LENGTH_SHORT).show()
+                }
+                R.id.dislike -> {
+                    Toast.makeText(this, "Уже нажато", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
