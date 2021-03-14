d805457f2fe5580a66322da90145984cc25f18d1
Nikita Balashov <n.balashov@crmsensor.ru>
2021-03-14 09:28:34 +0300

    feat: notyet item click

--- app/src/main/java/com/pavesid/niksl/ui/notyet/NotYetFragment.kt
+++ app/src/main/java/com/pavesid/niksl/ui/notyet/NotYetFragment.kt
@@ -2,7 +2,6 @@ package com.pavesid.niksl.ui.notyet
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +10,7 @@ import com.pavesid.niksl.R
 import com.pavesid.niksl.core.OverlapDecoration
 import com.pavesid.niksl.core.viewBinding
 import com.pavesid.niksl.databinding.FragmentDoneBinding
+import com.pavesid.niksl.ui.MainActivity
 import com.pavesid.niksl.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +20,7 @@ class NotYetFragment : Fragment(R.layout.fragment_done) {
     private val viewModel: MainViewModel by viewModels()
 
     private val binding: FragmentDoneBinding by viewBinding(FragmentDoneBinding::bind)
+    private val mainActivity by lazy { activity as MainActivity }
 
     private lateinit var notYetAdapter: NotYetAdapter
 
@@ -31,10 +32,12 @@ class NotYetFragment : Fragment(R.layout.fragment_done) {
     }
 
     private fun initView() {
-        notYetAdapter = NotYetAdapter {
-            viewModel.updateAchievement(true, it.id)
-            Toast.makeText(context, "Done", Toast.LENGTH_SHORT).show()
-        }
+        notYetAdapter = NotYetAdapter(
+            { achievement ->
+                viewModel.updateAchievement(true, achievement.id)
+            },
+            mainActivity::openDetails
+        )
         val moviesLayoutManager = LinearLayoutManager(context, VERTICAL, false)
         binding.rvDone.apply {
             setHasFixedSize(true)
